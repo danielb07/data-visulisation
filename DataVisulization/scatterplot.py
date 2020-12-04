8ed89e7fa5dae93485900ae11250a5041da4549d
danielb07 <danielbarraud07@gmail.com>
2020-12-04 19:08:26 +0530

    fixed bugs

--- DataVisulization/scatterplot.py
+++ DataVisulization/scatterplot.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.express as px
-
-df = pd.read_csv("owid-covid-data.csv")
-scatter = px.scatter(df,x="date",y="total_cases", size="Percentage", color="location",size_max=60)
+df = pd.read_csv(r"D:\learn2code\white-hat-jr\python\data-visulisation\DataVisulization\owid-covid-data.csv")
+scatter = px.scatter(df,x="date",y="total_cases", color="location",size_max=60)
 scatter.show()
